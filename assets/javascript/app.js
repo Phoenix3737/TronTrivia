dee9ac764c8cf0c1a59970f2e5bfd69660d6ce7a
mark groves <groves999@gmail.com>
2019-01-23 14:46:51 -0500

    added app json

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 var Trivia = Trivia || {};
 Trivia = (function () {
+    "use strict";
     var correctCount = 0;
     var wrongCount = 0;
     var unansweredCount = 0;
     var currentQuestionIndex = 0;
     var disabled = false;
     var x;
-    var data = [];
-    // HTML template using strings
+    var _questions = [];
+
     function getTemplate(item) {
-        // console.log(item)
         return '<div class="row">' +
             '<div class="col-xs-12">' +
             '<div class="question">' + item.question + '</div>' +
@@ -32,37 +32,28 @@ Trivia = (function () {
 
     function next() {
         currentQuestionIndex++;
-        if (currentQuestionIndex < data.length) {
-            $("#gameScreen").html(getTemplate(data[currentQuestionIndex]));
+        if (currentQuestionIndex < _questions.length) {
+            $("#gameScreen").html(getTemplate(_questions[currentQuestionIndex]));
         }
         else {
             clearInterval(x);
             $("#gameScreen").hide();
             $(".game-over").show();
             $('#clock').hide();
+            $("#score").show();
             $("#score").html("Correct answers: " + correctCount + " &nbsp; &nbsp; Incorrect answers: " + wrongCount + " &nbsp; &nbsp; Unanswered: " + unansweredCount);
-        };
+        }
         disabled = false;
     }
 
-    function registerClick() {
-                
+    function registerClick() {        
         var correct = "Correct!";
-        $(document).on("click", ".answers li", function () {
-                
+        $(document).on("click", ".answers li", function () {    
             if (!disabled) {
                 
                 clearInterval(x);
-                // console.log(x);
                 var index = $(this).index();
-                    console.log(index);
-                    // console.log(data);
-                    // console.log(currentQuestionIndex);
-                    // console.log(data[currentQuestionIndex]);
-
-                var correctAnswerIndex = data[currentQuestionIndex].correctAnswer;
-                // console.log(correctAnswerIndex);
-               
+                var correctAnswerIndex = _questions[currentQuestionIndex].correctAnswer;
                 if (index === correctAnswerIndex) {
                     correctCount++;
                     document.getElementById('clock').style.fontSize = "45px";
@@ -73,20 +64,17 @@ Trivia = (function () {
                     wrongCount++;
                     document.getElementById('clock').style.fontSize = "45px";
                     document.getElementById('clock').style.color = "red";
-                    showMessage("Wrong! " + data[currentQuestionIndex].answers[correctAnswerIndex]);
+                    showMessage("Wrong! " + _questions[currentQuestionIndex].answers[correctAnswerIndex]);
                 }
                 disabled = true;
             }
         });
     }
 
-    function play(data) {
-        this.data = data;
-        $("#gameScreen").html(getTemplate(data[0]));
+    function registerEvents() {
         registerClick();
-        $(".game-over, #gameScreen").hide()
         $("#start-btn").click(function () {
-            // $("#music")[0].play();
+            $("#music")[0].play();
             $("#mute").show();
             $("#gameScreen").show();
             $(this).hide();
@@ -99,11 +87,24 @@ Trivia = (function () {
             unansweredCount = 0;
             currentQuestionIndex = 0;
             disabled = false;
-            data = [];
+            play(_questions);
+            $("#score").hide();
+            $("#gameScreen").show();
+            $("#clock").show();
+            timer();
 
-
-            // window.location.reload();
         });
+    }
+
+    function init (questions) {
+        _questions = questions;
+        registerEvents();
+    }
+
+    function play(questions) {
+        $("#gameScreen").html(getTemplate(questions[0]));
+        $(".game-over, #gameScreen").hide();
+
 
     }
 
@@ -137,19 +138,18 @@ Trivia = (function () {
         }, 4000);
     }
     return {
-        play: play
+        play: play,
+        init: init
     };
 })();
 
-
-
 $(document).ready(function () {
     $.get("/api/questions", function (questions) {
+        Trivia.init(questions);
         Trivia.play(questions);
-        // console.log(questions[0].correctAnswer);
     });
-    
-    $("#mute").hide();
+
+ $("#mute").hide();
     $("#mute").click(function (e) {
         e.preventDefault();
         var song = $('audio')[0]
@@ -163,65 +163,3 @@ $(document).ready(function () {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// {
-    //     question: "What actor played the main character, Kevin Flynn/Clu, in the film?",
-    //     answers:[
-    //         "Beau Bridges",
-    //         "Jeff Bridges",
-    //         "London Bridges",
-    //         "Harrison Ford"
-    //     ],
-    //     correctAnswer: 1
-    // },
-    // {
-    //     question: "The star of TRON, Jeff Bridges, also played what other classic cult hit character?",
-    //     answers:[
-    //         "Flash Gordon",
-    //         "Han Solo",
-    //         "Mad Max",
-    //         "The Dude"
-    //     ],
-    //     correctAnswer: 3
-    // },
-    // {
-    //     question: "In What year did TRON hit theaters?",
-    //     answers:[
-    //         "1982",
-    //         "1978",
-    //         "1984",
-    //         "1999"
-    //     ],
-    //     correctAnswer: 0
-    // },
-    // {
-    //     question: "The sequel to the film was named...?",
-    //     answers:[
-    //         "TRON: Play Again",
-    //         "TRON: Game Over",
-    //         "TRON: Legacy",
-    //         "TRONS"  
-    //     ],
-    //     correctAnswer: 2
-    // }
--- server/data/questions.js
+++ server/data/questions.js
@@ -1,5 +1,4 @@
-var questions = [ // correctAnwer is undefined because it's in here and no longer reachable.
-                // Maybe (key: value)  pairs in app.js aka  question[0]: correctAnswer: 1
+var data = [
     {
         question: "Why was TRON disqualified from receiving an Academy Award nomination for special effects?",
         answers:[
@@ -20,89 +19,89 @@ var questions = [ // correctAnwer is undefined because it's in here and no longe
         ],
         correctAnswer: 3
     },
-    // {
-    //     question: "Flynn's game program is named CLU, which is an acronym for?",
-    //     answers:[
-    //         "Crispy Lightly-breaded Unicorns",
-    //         "Computed Life-like Uni-code",
-    //         "Codified Likeness Utility",
-    //         "Computer Life Unity"
-    //     ],
-    //     correctAnswer: 2
-    // },
-    // {
-    //     question: "Ram's light cycle in the book is green, but in the movie it rezzes up as what color?",
-    //     answers:[
-    //         "A lovely shade of Chartreuse.",
-    //         "Blue",
-    //         "Gold",
-    //         "Red"
-    //     ],
-    //     correctAnswer: 3
-    // },
-    // {
-    //     question: "Who was the film's director?",
-    //     answers:[
-    //         "Steven Speilberg",
-    //         "George Lucas",
-    //         "Steven Lisberger",
-    //         "Ron Howard"
-    //     ],
-    //     correctAnswer: 2
-    // },
-    // {
-    //     question: "Who wrote the novelization of TRON?",
-    //     answers:[
-    //         "Ernest Hemmingway",
-    //         "Brian Daley",
-    //         "Gregory Benford",
-    //         "J. K. Rowling"
-    //     ],
-    //     correctAnswer: 1
-    // },
-    // {
-    //     question: "What actor played the main character, Kevin Flynn/Clu, in the film?",
-    //     answers:[
-    //         "Beau Bridges",
-    //         "Jeff Bridges",
-    //         "London Bridges",
-    //         "Harrison Ford"
-    //     ],
-    //     correctAnswer: 1
-    // },
-    // {
-    //     question: "The star of TRON, Jeff Bridges, also played what other classic cult hit character?",
-    //     answers:[
-    //         "Flash Gordon",
-    //         "Han Solo",
-    //         "Mad Max",
-    //         "The Dude"
-    //     ],
-    //     correctAnswer: 3
-    // },
-    // {
-    //     question: "In What year did TRON hit theaters?",
-    //     answers:[
-    //         "1982",
-    //         "1978",
-    //         "1984",
-    //         "1999"
-    //     ],
-    //     correctAnswer: 0
-    // },
-    // {
-    //     question: "The sequel to the film was named...?",
-    //     answers:[
-    //         "TRON: Play Again",
-    //         "TRON: Game Over",
-    //         "TRON: Legacy",
-    //         "TRONS"  
-    //     ],
-    //     correctAnswer: 2
-    // }
+    {
+        question: "Flynn's game program is named CLU, which is an acronym for?",
+        answers:[
+            "Crispy Lightly-breaded Unicorns",
+            "Computed Life-like Uni-code",
+            "Codified Likeness Utility",
+            "Computer Life Unity"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Ram's light cycle in the book is green, but in the movie it rezzes up as what color?",
+        answers:[
+            "A lovely shade of Chartreuse.",
+            "Blue",
+            "Gold",
+            "Red"
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "Who was the film's director?",
+        answers:[
+            "Steven Speilberg",
+            "George Lucas",
+            "Steven Lisberger",
+            "Ron Howard"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Who wrote the novelization of TRON?",
+        answers:[
+            "Ernest Hemmingway",
+            "Brian Daley",
+            "Gregory Benford",
+            "J. K. Rowling"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "What actor played the main character, Kevin Flynn/Clu, in the film?",
+        answers:[
+            "Beau Bridges",
+            "Jeff Bridges",
+            "London Bridges",
+            "Harrison Ford"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "The star of TRON, Jeff Bridges, also played what other classic cult hit character?",
+        answers:[
+            "Flash Gordon",
+            "Han Solo",
+            "Mad Max",
+            "The Dude"
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "In What year did TRON hit theaters?",
+        answers:[
+            "1982",
+            "1978",
+            "1984",
+            "1999"
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "The sequel to the film was named...?",
+        answers:[
+            "TRON: Play Again",
+            "TRON: Game Over",
+            "TRON: Legacy",
+            "TRONS"  
+        ],
+        correctAnswer: 2
+    }
 ];
 
-module.exports = questions;
+module.exports = data;
 
 
 
